d143c0bd40f9f93eb7c0a2ed7b4edb43891a76ed
Stephen Dicks <github@starswan.com>
2023-01-23 17:09:26 +0000

    EL-604 Add tests (and fixes where appropriate) for all capital items payloads

--- app/services/decorators/v5/property_decorator.rb
+++ app/services/decorators/v5/property_decorator.rb
@@ -13,17 +13,17 @@ module Decorators
 
       def payload
         {
-          value: @record.value,
-          outstanding_mortgage: @record.outstanding_mortgage,
-          percentage_owned: @record.percentage_owned,
+          value: @record.value.to_f,
+          outstanding_mortgage: @record.outstanding_mortgage.to_f,
+          percentage_owned: @record.percentage_owned.to_f,
           main_home: @record.main_home,
           shared_with_housing_assoc: @record.shared_with_housing_assoc,
-          transaction_allowance: @record.transaction_allowance,
-          allowable_outstanding_mortgage: @record.allowable_outstanding_mortgage,
-          net_value: @record.net_value,
-          net_equity: @record.net_equity,
-          main_home_equity_disregard: @record.main_home_equity_disregard,
-          assessed_equity: @record.assessed_equity,
+          transaction_allowance: @record.transaction_allowance.to_f,
+          allowable_outstanding_mortgage: @record.allowable_outstanding_mortgage.to_f,
+          net_value: @record.net_value.to_f,
+          net_equity: @record.net_equity.to_f,
+          main_home_equity_disregard: @record.main_home_equity_disregard.to_f,
+          assessed_equity: @record.assessed_equity.to_f,
         }
       end
     end
