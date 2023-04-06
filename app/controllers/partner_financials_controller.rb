84032de5fc0bbda2d1b69714df74f4ff8c74568b
Stephen Dicks <github@starswan.com>
2023-04-06 08:55:36 +0100

    DRY Partner controller

--- app/controllers/partner_financials_controller.rb
+++ app/controllers/partner_financials_controller.rb
@@ -1,25 +1,14 @@
-class PartnerFinancialsController < ApplicationController
+class PartnerFinancialsController < CreationController
   def create
-    json_validator = JsonSwaggerValidator.new("partner_financials", partner_financials_params)
-    if json_validator.valid?
-      if creation_service.success?
-        render_success
-      else
-        render_unprocessable(creation_service.errors)
-      end
-    else
-      render_unprocessable(json_validator.errors)
-    end
+    create_object "partner_financials", partner_financials_params, lambda {
+      Creators::PartnerFinancialsCreator.call(
+        assessment_id: params[:assessment_id],
+        partner_financials_params:,
+      )
+    }
   end
 
 private
-
-  def creation_service
-    @creation_service ||= Creators::PartnerFinancialsCreator.call(
-      assessment_id: params[:assessment_id],
-      partner_financials_params:,
-    )
-  end
 
   def partner_financials_params
     JSON.parse(request.raw_post, symbolize_names: true)
