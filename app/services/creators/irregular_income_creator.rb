9cc7cbe75f38d1ab3540c55e5975df1fec9ae9d6
Stephen Dicks <github@starswan.com>
2023-04-06 08:04:12 +0100

    tidy up IrregularIncomeCreator

--- app/services/creators/irregular_income_creator.rb
+++ app/services/creators/irregular_income_creator.rb
@@ -4,18 +4,15 @@ module Creators
       CreationResult = Data.define(:success?)
 
       def call(irregular_income_params:, gross_income_summary:)
-        create_records irregular_income_params[:payments], gross_income_summary
-        CreationResult.new(success?: true)
-      end
-
-      def create_records(irregular_income_payments, gross_income_summary)
-        irregular_income_payments.each do |payment_params|
+        irregular_income_params[:payments].each do |payment_params|
           gross_income_summary.irregular_income_payments.create!(
             income_type: payment_params[:income_type],
             frequency: payment_params[:frequency],
             amount: payment_params[:amount],
           )
         end
+
+        CreationResult.new(success?: true)
       end
     end
   end
